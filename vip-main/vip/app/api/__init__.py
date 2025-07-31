846238e090afb212fc7dc4093422d0192910945c
avsize8 <158048434+avsize8@users.noreply.github.com>
2025-07-31 09:27:07 +0300

    Delete vip-main directory

--- vip-main/vip/app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Создаем Blueprint для API v1
-api_v1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
-
-# Импортируем все маршруты API
-from . import applications, health 
